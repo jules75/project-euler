a6cb45f4e5903b51f2b55ed30f1fac29c1190692
Julian Laffey <julianlaffey@gmail.com>
2013-12-31 08:56:06 +1100

    Solution 22

--- src/euler/p20.clj
+++ src/euler/p20.clj
@@ -12,3 +12,13 @@
 			(if (and (not= n m) (= n (->> m f/proper-divisors (reduce +))))
 				n)
 				)))))
+
+(defn p22 []
+	(let [sorted-names (-> (slurp "names.txt") 
+			(clojure.string/split #"\"*,*\"") 	;;;")
+			sort vec)
+		scores (->> sorted-names
+			(map #(map (fn [s] (- (int (char s)) (dec (int \A)))) %))
+			(map #(reduce + %)))]
+		(reduce + (map * scores (range)))
+		))
