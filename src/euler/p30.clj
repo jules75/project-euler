4fbfbd0d9b74b3cd15282445e57b59e52b6250e7
Julian Laffey <julianlaffey@gmail.com>
2014-01-02 13:14:39 +1100

    Solution 33

--- src/euler/p30.clj
+++ src/euler/p30.clj
@@ -1,5 +1,6 @@
 (ns euler.p30
 	(:require [euler.fns :as f]
+		[clojure.set :as set]
 		[clojure.math.combinatorics :as c]))
 
 (defn p30 []
@@ -27,3 +28,22 @@
 				(map #(= (range 1 10) (sort (mapcat f/digits %))))
 				(some true?) boolean) n 0
 				)))))
+
+(defn p33 []
+	(letfn [
+		(remove-first [n coll]
+			(let [[a b] (map vec (split-with #(not= n %) coll))]
+				(into a (rest b))))
+		(undigits [coll]
+			(if (seq coll) (Integer/parseInt (apply str coll)) 0))
+		(shared-digits [n m]
+			(set/intersection (set (f/digits n)) (set (f/digits m))))]
+	(->>
+		(for [a (range 1 100) b (range 1 100) :when (and (< a b) (pos? (rem a 10)))]
+			(let [shared (first (shared-digits a b))
+				[aa bb] (map #(->> % f/digits (remove-first shared) undigits) [a b])]
+				(if (and (pos? bb) (not= a aa) (= (/ a b) (/ aa bb)))
+					[a b])))
+		(remove nil?)
+		(map #(/ (first %) (last %)))
+		(reduce *))))
