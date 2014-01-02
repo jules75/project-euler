63c634a9fc4e72d8ed8f4f381f1dcf9a9f35e6ef
Julian Laffey <julianlaffey@gmail.com>
2014-01-02 23:23:12 +1100

    Solution 41

--- src/euler/p40.clj
+++ src/euler/p40.clj
@@ -1,8 +1,14 @@
 (ns euler.p40
-	(:require [euler.fns :as f]))
+	(:require [euler.fns :as f]
+		[clojure.math.combinatorics :as c]))
 
 (defn p40 []
 	(let [ch (apply str (range 200000))
 		d (map #(reduce *' (repeat % 10)) (range 7))]
 		(->>(map #(->> % (nth ch) str Integer/parseInt) d)
 			f/undigits f/digits (apply *))))
+
+(defn p41 []	
+	(let [len 7	; all 8 or 9 digit pandigitals are divisible by 9
+		pandigitals (->> (range 1 (inc len)) c/permutations reverse (map f/undigits))]
+		(first (filter f/prime? pandigitals))))
