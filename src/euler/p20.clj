ce512d2ae2bd14862e0585c08dc0e5a047dbe33e
Julian Laffey <julianlaffey@gmail.com>
2014-01-09 22:30:55 +1100

    Solution 26

--- src/euler/p20.clj
+++ src/euler/p20.clj
@@ -41,7 +41,21 @@
 		(filter #(= 1000 (count (str (last %)))))
 		first first))
 
-(defn p26 [] nil)	; TODO (see git stash)
+(defn p26 []
+; 
+; To calculate the length of period 1/p where p is prime, check if it is
+; evenly divisible into 9, then 99, then 999, etc. When you find one divisible 
+; by p, the number of 9s is period length.
+;
+; See point 9 at http://mathworld.wolfram.com/DecimalExpansion.html
+; 
+	(let [nines (iterate #(+ 9 (*' 10 %)) 9)
+		primes (drop 3 (take-while #(< % 1000) (f/primes)))]
+	(->>
+		(for [p primes] (-> (filter #(zero? (rem % p)) nines) first str count))
+		(zipmap primes)
+		(sort-by val)
+		last first)))
 
 (defn p27 []
 	(->> (for [a (range -1000 1000) b (range -1000 1000)] 
