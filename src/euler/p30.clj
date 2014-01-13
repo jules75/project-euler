ea3fc1c45cc6d93e67af0fe0d28a7084d9c41cc2
Julian Laffey <julianlaffey@gmail.com>
2014-01-13 18:13:22 +1100

    Improve performance of problem 35

--- src/euler/p30.clj
+++ src/euler/p30.clj
@@ -1,11 +1,25 @@
 (ns euler.p30
 	(:require [euler.fns :as f]))
+		
+(def pow5 {	; faster than calculating
+	0 0
+	1 1
+	2 32
+	3 243
+	4 1024
+	5 3125
+	6 7776
+	7 16807
+	8 32768
+	9 59049})
 
-(defn sum5p 
-	[n] 
-	(reduce + (map #(reduce * (repeat 5 %)) (f/digits n))))
+(defn sum-digits-5th-power?
+	[n]
+	"True if each digit to the power of 5 adds up to n"
+	(= n (reduce + (map #(get pow5 %) (f/digits n)))))
 	
 (defn p30 []
-	(->> (range 2 999999)	; TODO smart way to find upper limit
-		(filter #(= % (sum5p %)))
-		(reduce +)))
+	(->> (range 2 200000)	; TODO smarter way to find upper limit
+		(filter sum-digits-5th-power?)
+		(reduce +)
+		))
