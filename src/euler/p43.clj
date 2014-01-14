bd13afeb9adc31e903cc2003638a5d5d350e87a1
Julian Laffey <julianlaffey@gmail.com>
2014-01-14 15:03:57 +1100

    p43 time down from 45s to under 100ms

--- src/euler/p43.clj
+++ src/euler/p43.clj
@@ -1,18 +1,82 @@
 (ns euler.p43
 	(:require [euler.fns :as f]
-		[clojure.math.combinatorics :as c]))
+		[clojure.set :as s]))
 
-(defn candidate? 
-	[digits]
-	(let [primes [2 3 5 7 11 13 17]
-		groups (map #(f/undigits (subvec digits % (+ % 3))) (range 1 8))
-		remainders (map #(rem % %2) groups primes)]
-		(every? zero? remainders)))
+(defn unique-digits?
+	[n]
+	"True if n has no repeated digits"
+	(let [d (f/digits n)]
+		(= (count d) (count (distinct d)))))
+		
+(defn init 
+	[]
+	"All 3 digit multiples of 17 with unique digits"
+	(->> (rest (map #(* 17 %) (range)))
+		(take-while #(< % 1000))
+		(filter unique-digits?)
+		(map vector)))
 
+(defn prepend-multiple
+	[n m]
+	"Returns all multiple of m that;
+		- are 3 (or fewer) digit numbers 
+		- contain unique digits 
+		- have last 2 digits that are the first 2 digits of 3-digit n"
+	(->> (map #(+ % (int (/ n 10))) (range 0 1000 100))
+		(filter unique-digits?)
+		(filter #(zero? (rem % m)))
+		))
+
+(defn prepend-missing
+	[n]
+	"Given 9-digit n that contains only unique digits, prepend the 
+	missing digit, e.g. 106357289 => 4106357289"
+	(f/undigits
+		(let [digits (f/digits n)]
+			(cons (first (s/difference (set (range 10)) (set digits))) digits)
+			)))
+
+(defn assemble
+	[coll]
+	"Re-assemble numbers, e.g. 231 319 195 952 => 231952"
+	(f/undigits
+		(let [digits (map f/digits coll)]
+			(concat (first digits) (map last (rest digits)))
+			)))
+
+; ugly as hell, but MUCH faster than brute force
+; TODO refactor
 (defn p43 []
-	(->> (c/permutations (range 10))
-		(filter #(even? (nth % 3)))						; 4th digit must be even
-		(filter #(or (= 0 (nth % 5)) (= 5 (nth % 5))))	; 6th digit must be 0 or 5
-		(filter candidate?)
-		(map f/undigits)
-		(reduce +)))
+	(->> 
+		(init) 
+		
+		(remove #(empty? (prepend-multiple (first %) 13)))
+		(mapcat #(for [n (prepend-multiple (first %) 13)] (cons n %)))
+		(filter #(unique-digits? (assemble %)))
+		
+		(remove #(empty? (prepend-multiple (first %) 11)))
+		(mapcat #(for [n (prepend-multiple (first %) 11)] (cons n %)))
+		(filter #(unique-digits? (assemble %)))
+		
+		(remove #(empty? (prepend-multiple (first %) 7)))
+		(mapcat #(for [n (prepend-multiple (first %) 7)] (cons n %)))
+		(filter #(unique-digits? (assemble %)))
+		
+		(remove #(empty? (prepend-multiple (first %) 5)))
+		(mapcat #(for [n (prepend-multiple (first %) 5)] (cons n %)))
+		(filter #(unique-digits? (assemble %)))
+		
+		(remove #(empty? (prepend-multiple (first %) 3)))
+		(mapcat #(for [n (prepend-multiple (first %) 3)] (cons n %)))
+		(filter #(unique-digits? (assemble %)))		
+		
+		(remove #(empty? (prepend-multiple (first %) 2)))
+		(mapcat #(for [n (prepend-multiple (first %) 2)] (cons n %)))
+		(filter #(unique-digits? (assemble %)))		
+
+		(map assemble)
+		(remove #(< (count (f/digits %)) 9))
+		(map prepend-missing)
+		(reduce +)
+		
+		))
