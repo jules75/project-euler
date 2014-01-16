073c1c2e6f4830d8294cf1662ab14f4e5617bdf3
Julian Laffey <julianlaffey@gmail.com>
2014-01-17 03:23:55 +1100

    Reduced run time of solution 52 to a third

--- src/euler/p52.clj
+++ src/euler/p52.clj
@@ -7,8 +7,9 @@
 	(apply = (map #(frequencies (f/digits %)) coll)))
 	
 (defn p52 []
+	(let [start 100000]	; must be a 6+ digit number
 	(->>
-		(for [n (rest (range))] (map #(* n %) (range 1 7)))
+		(for [n (iterate inc start)] (map #(* n %) (range 1 7)))
 		(filter same-digits?)
 		first
-		first))
+		first)))
