c1104c61f95cb206d7d273e58dda9e8181c5647d
Julian Laffey <julianlaffey@gmail.com>
2014-04-05 11:43:00 +1100

    Refactor: tidy up some fns

--- src/euler/p54.clj
+++ src/euler/p54.clj
@@ -1,19 +1,14 @@
 (ns euler.p54
-	(:require [euler.fns :as f]
-			[clojure.pprint :as pp]
-			[clojure.math.numeric-tower :as math]
-			[clojure.string :as s]))
-			
-(def values { \2 2 \3 3 \4 4 \5 5 \6 6 \7 7 \8 8 \9 9 \T 10 \J 11 \Q 12 \K 13 \A 14})
-	
-(def suits { \H :hearts \D :diamonds \C :clubs \S :spades})
-			
+	(:require [clojure.string :as s]))
+
 (defn cardify
 	[s]
 	"Turns 2 char string (e.g. AS, 5H) into playing card map"
-	{:suit (suits (last s))
-	:value (values (first s))
-	:str s})
+    (let [values {\2 2 \3 3 \4 4 \5 5 \6 6 \7 7 \8 8 \9 9 \T 10 \J 11 \Q 12 \K 13 \A 14}
+          suits {\H :hearts \D :diamonds \C :clubs \S :spades}]
+        {:suit (suits (last s))
+        :value (values (first s))
+        :str s}))
 
 (defn fetch-hand-pairs
 	[]
@@ -24,15 +19,15 @@
 		(map #(s/split % #"\s"))
 		(map #(map cardify %))
 		(map #(split-at 5 %))))
-		
+
 (defn flush?
 	[hand]
 	(apply = (map :suit hand)))
-	
+
 (defn straight?
 	[hand]
 	(let [values (distinct (sort (map :value hand)))]
-		(and 
+		(and
 			(= 5 (count values))					; 5 different values
 			(= 4 (- (last values) (first values)))	; each 1 apart
 			)))
@@ -43,10 +38,10 @@
 
 (defn royal-flush?
 	[hand]
-	(and 
+	(and
 		(straight-flush? hand)
 		(= 14 (->> hand (map :value) sort last))))
-	
+
 (defn full-house?
 	[hand]
 	(= 2 (count (frequencies (map :value hand)))))
@@ -57,7 +52,7 @@
 		frequencies
 		(some #(= n (val %)))
 		boolean))
-		
+
 (defn two-pairs?
 	[hand]
 	(->> (map :value hand)
@@ -80,7 +75,7 @@
 		(two-pairs? hand)		3
 		(n-of-a-kind? hand 2)	2
 		:else					1))
-		
+
 (defn sort-hand
 	[hand]
 	"Returns hand sorted by rank then by value"
@@ -93,20 +88,20 @@
 (defn highest?
 	[hand1 hand2]
 	"True if hand1 has higher value than hand2
-	Hands MUST be sorted with sort-hand before being passed
-	Test only reliable if hands have same rank"
-	(let [h1 (map :value hand1) h2 (map :value hand2)]
+	Test only reliable if hands are same rank"
+	(let [h1 (map :value (sort-hand hand1))
+          h2 (map :value (sort-hand hand2))]
 		(cond
 			(= (first h1) (first h2)) (recur (rest hand1) (rest hand2))
 			(> (first h1) (first h2)) true
 			:else false)))
-			
+
 (defn p54 []
 	(count (filter true?
 	(for [[h1 h2] (fetch-hand-pairs)]
 		(cond
 			(> (rank h1) (rank h2)) true
 			(< (rank h1) (rank h2)) false
-			(= (rank h1) (rank h2)) (highest? (sort-hand h1) (sort-hand h2))
+			(= (rank h1) (rank h2)) (highest? h1 h2)
 			)))))
-
+
