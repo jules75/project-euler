387f393d3cbfac5fc9bf4d43adf3a068ab0e57ab
Julian Laffey <fattyjules@yhaoo.com.au>
2014-04-16 08:58:05 +1000

    Problem 58

---
+++ src/euler/p58.clj
@@ -0,0 +1,38 @@
+(ns euler.p58
+	(:require [clojure.string :as s]))
+
+(def cipher-nums
+  (let [raw (slurp "https://projecteuler.net/project/cipher1.txt")
+   tokens (s/split (s/trim raw) #",")]
+  (map #(Integer/parseInt %) tokens)
+  ))
+
+(def passwords
+  "Lazy sequence of all possible passwords (as numbers)"
+  (let [nums (range 97 123)]
+    (for [a nums b nums c nums]
+      (vector a b c)
+      )))
+
+(defn xor
+  [cipher password]
+  "Cipher and password are integers, password will be repeated
+  Returns integers"
+  (map bit-xor cipher (cycle password))
+  )
+
+(defn looks-english?
+  [string]
+  (boolean (re-find #" the " string))
+  )
+
+(defn p58 []
+  (->> passwords
+   (map #(xor cipher-nums %))
+   (map #(map char %))
+   (map #(apply str %))
+   (filter looks-english?)
+   first
+   (map int)
+   (apply +)
+   ))
