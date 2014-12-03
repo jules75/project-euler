13dae48f58c16d97bd427d927279d16c5900848b
Julian Laffey <julianlaffey@gmail.com>
2014-12-04 00:20:59 +1100

    Threefold speed increase by generating square digits sums instead of calculating them

--- src/euler/p92.clj
+++ src/euler/p92.clj
@@ -19,8 +19,7 @@
   [n lookup]
   (let [m (square-digit n)
 		t (get lookup m)]
-	(if t t (recur m lookup))
-	))
+	(if t t (recur m lookup))))
 
 
 (defn populate-lookup!
@@ -28,20 +27,34 @@
   under 1000 when n < 1e7."
   []
   (doseq [n (range 1 (-> LIMIT Math/log10 int inc (* 9 9)))]
-	(swap! term-lookup #(assoc % n (terminator n @term-lookup)))
-	))
+	(swap! term-lookup #(assoc % n (terminator n @term-lookup)))))
+
+
+(defn boundary
+  [n]
+  (let [[_ pp mm] (re-find #"([0-8]{0,1})(9+)$" (str n))
+		m (count mm)
+		p (if (seq pp) (Integer/parseInt pp) 0)]
+	(* -1 (+ (* 81 m) (* p p) (* -1 (inc p) (inc p))))))
+
+
+(defn square-digit-sums
+  "Returns an infinte sequence of all square digit sums, starting
+  with 1. Takes advantage of pattern of differences between each
+  value, much faster than fully calculating each one."
+  []
+  (let [tens (map #(-> % inc (* 10) dec boundary) (range))]
+	(reductions + (mapcat #(vector 1 3 5 7 9 11 13 15 17 %) tens))))
 
 
 (defn p92
   []
   (populate-lookup!)
-  (->>
-   (range 1 LIMIT)
-   (map #(get @term-lookup (square-digit %)))
-   (filter #(= 89 %))
-   count
-   ))
+  (->> (square-digit-sums)
+	   (map #(get @term-lookup %))
+	   (take LIMIT)
+	   (filter #(= 89 %))
+	   count))
 
 
 ;(time (p92))
-
