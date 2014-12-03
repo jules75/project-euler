5bd710f7a2a106d9a36260ac99bb515b2b8d837f
Julian Laffey <julianlaffey@gmail.com>
2014-12-03 18:05:18 +1100

    Solution 92 (slow)

---
+++ src/euler/p92.clj
@@ -0,0 +1,43 @@
+(ns euler.p92
+  (:require [euler.fns :as f]))
+
+
+(defn square-digit
+  "Returns sum of the squares of every digit in n."
+  [n]
+  (->> n f/digits (map #(* % %)) (reduce +)))
+
+
+(defn terminator
+  "Returns terminator (1 or 89) of square digit chain for n. A lookup map
+  of known termination values can be provided to run faster, i.e. {1 1, 89, 89}.
+  Passing an empty lookup will hang function."
+  [n lookup]
+  (if-let [t (get lookup n)]
+	t
+	(recur (square-digit n) lookup)
+	))
+
+
+; terminator lookup table for performance
+(def term-lookup (atom {1 1, 89 89}))
+
+
+(defn populate-lookup!
+  "Populate lookup table for n up to 1000."
+  []
+  (doseq [n (range 1 1000)]
+	(swap! term-lookup #(assoc % n (terminator n @term-lookup)))
+	))
+
+
+(defn p97
+  []
+  (populate-lookup!)
+  (->>
+   (range 1 1e7)
+   (map #(terminator % @term-lookup))
+   (filter #(= 89 %))
+   count
+   ))
+
