ac61fca49836ad6268bc3282c95996a9dd5fbf36
Julian Laffey <julianlaffey@gmail.com>
2014-12-03 22:11:45 +1100

    Put limit in const

--- src/euler/p92.clj
+++ src/euler/p92.clj
@@ -2,8 +2,8 @@
   (:require [euler.fns :as f]))
 
 
-; terminator lookup table for performance
 (def term-lookup (atom {1 1, 89 89}))
+(def LIMIT 1e7)
 
 
 (defn square-digit
@@ -17,7 +17,6 @@
   of known termination values can be provided to run faster, i.e. {1 1, 89, 89}.
   Passing an empty lookup will hang function."
   [n lookup]
-  (swap! counter inc)
   (let [m (square-digit n)
 		t (get lookup m)]
 	(if t t (recur m lookup))
@@ -28,7 +27,7 @@
   "Populate lookup table for n up to 1000. (square-digit n) always comes in
   under 1000 when n < 1e7."
   []
-  (doseq [n (range 1 1000)]
+  (doseq [n (range 1 (-> LIMIT Math/log10 int inc (* 9 9)))]
 	(swap! term-lookup #(assoc % n (terminator n @term-lookup)))
 	))
 
@@ -37,12 +36,12 @@
   []
   (populate-lookup!)
   (->>
-   (range 1 1e7)
+   (range 1 LIMIT)
    (map #(get @term-lookup (square-digit %)))
    (filter #(= 89 %))
    count
    ))
 
 
-(time (p92))
+;(time (p92))
 
