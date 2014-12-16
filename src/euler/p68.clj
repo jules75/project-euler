a9fe61982fa77f55eb752081b2415f3d3783ca39
Julian Laffey <julianlaffey@gmail.com>
2014-12-16 11:45:53 +1100

    P68: slight efficiency increase

--- src/euler/p68.clj
+++ src/euler/p68.clj
@@ -4,20 +4,25 @@
 			[clojure.math.combinatorics :as c]))
 
 
+(require '[taoensso.timbre.profiling :refer (profile p)])
+
+
 (defn split-when
   [pred coll]
-  (vector
-   (take-while (complement pred) coll)
-   (drop-while (complement pred) coll)
-   ))
+  (p :split-when
+	 (vector
+	  (take-while (complement pred) coll)
+	  (drop-while (complement pred) coll)
+	  )))
 
 
 (defn to-5-gon
   "Return 5-gon from set of numbers."
   [coll]
-  (let [idx [0 1 2 3 2 4 5 4 6 7 6 8 9 8 1]]
-	(partition 3 (map (partial nth coll) idx))
-	))
+  (p :to-5-gon
+	 (let [idx [0 1 2 3 2 4 5 4 6 7 6 8 9 8 1]]
+	   (partition 3 (map (partial nth coll) idx))
+	   )))
 
 
 (defn min-rotate
@@ -25,22 +30,37 @@
   the collections until the one with the lowest first value
   is the first collection."
   [coll]
-  (let [m (apply min (map first coll))
-		chunks (split-when #(= m (first %)) coll)]
-	(apply concat (reverse chunks))
-	))
+  (p :min-rotate
+	 (let [m (apply min (map first coll))
+		   chunks (split-when #(= m (first %)) coll)]
+	   (apply concat (reverse chunks))
+	   )))
+
+
+(defn sums-equal?
+  "True if sums of first n groups in coll are equal.
+  Does it longer way to bail out early if needed."
+  [coll]
+  (p :sums-equal
+	 (let [sums (map #(apply + %) coll)
+		   m (first sums)]
+	   (every? true? (map #(= m %) sums))
+	 )))
 
 
 (defn p68
   []
   (->>
-   (c/permutations (range 1 11)) 		; all possible 1-10 permutations
-   (map to-5-gon) 						; turn into 5-gon structures
-   (map min-rotate) 					; satisfy 'lowest external node first'
-   (filter #(apply = (map (partial apply +) %))) 	; satisfy 'magic' requirement
-   (map (comp (partial apply str) flatten)) 		; turn into string
-   (filter #(= 16 (count %))) 			; must be 16 digits
-   (map bigint) 						; turn back into numbers
-   (reduce max) 						; find biggest
+   (c/permutations (range 1 11)) 			; all possible 1-10 permutations
+   (map to-5-gon) 							; turn into 5-gon structures
+   (filter sums-equal?) 					; satisfy 'magic' requirement
+   (map min-rotate) 						; satisfy 'lowest external node first'
+   (map (comp (partial apply str) flatten)) ; turn into string
+   (filter #(= 16 (count %))) 				; must be 16 digits
+   (map bigint) 							; turn back into numbers
+   (reduce max) 							; find biggest
    ))
+
+
+(profile :info :Arithmetic (p68))
 
