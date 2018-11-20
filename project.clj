754fb192e24dbc2db969cdbfa463edf844381290
Julian Laffey <julianlaffey@gmail.com>
2018-11-20 22:06:24 +1100

    gitignore, whitespace

--- project.clj
+++ project.clj
@@ -2,9 +2,9 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-			:url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
-				 [org.clojure/clojure "1.5.1"]
-				 [org.clojure/math.combinatorics "0.0.7"]
-				 [com.taoensso/timbre "3.3.1"]
-				 [org.clojure/math.numeric-tower "0.0.3"]])
+                 [org.clojure/clojure "1.5.1"]
+                 [org.clojure/math.combinatorics "0.0.7"]
+                 [com.taoensso/timbre "3.3.1"]
+                 [org.clojure/math.numeric-tower "0.0.3"]])
--- src/euler/fns.clj
+++ src/euler/fns.clj
@@ -1,130 +1,130 @@
 (ns euler.fns)
 
 (defn any-divisible?
-	[n coll]
-	"True if n is evenly divisible by ANY integer in coll"
-	(some zero? (map #(rem n %) coll)))
+  [n coll]
+  "True if n is evenly divisible by ANY integer in coll"
+  (some zero? (map #(rem n %) coll)))
 
 (defmulti palindrome? class)
 
 (defmethod palindrome? String
-	[s]
-	"True if string s is a palindrome"
-	(= s (->> s reverse (apply str))))
+  [s]
+  "True if string s is a palindrome"
+  (= s (->> s reverse (apply str))))
 
 (defmethod palindrome? Long
-	[n]
-	"True if integer n is a palindrome"
-	(palindrome? (str n)))
+  [n]
+  "True if integer n is a palindrome"
+  (palindrome? (str n)))
 
 (defn fibonacci
-	[]
-	"Returns infinite terms of fibonacci series"
-	(->> [0 1]
-		(iterate #(vector (last %) (+' (first %) (last %))))
-		(map first)))
+  []
+  "Returns infinite terms of fibonacci series"
+  (->> [0 1]
+       (iterate #(vector (last %) (+' (first %) (last %))))
+       (map first)))
 
-(defn factor	; TODO improve performance
-	([n m factors]
-	"Recursively test is m is factor of n, add to list of factors"
-	(if (> m (Math/sqrt n))
-		(conj factors n)
-		(if (zero? (rem n m))
-			(recur (/ n m) m (conj factors m))
-			(recur n (if (= 2 m) 3 (+ 2 m)) factors)
-		)))
-	([n]
-	"Return list of factors of n"
-	(factor n 2 [])))
+(defn factor                                                ; TODO improve performance
+  ([n m factors]
+   "Recursively test is m is factor of n, add to list of factors"
+   (if (> m (Math/sqrt n))
+     (conj factors n)
+     (if (zero? (rem n m))
+       (recur (/ n m) m (conj factors m))
+       (recur n (if (= 2 m) 3 (+ 2 m)) factors)
+       )))
+  ([n]
+   "Return list of factors of n"
+   (factor n 2 [])))
 
 (defn sq
-	[n]
-	"Square n"
-	(* n n))
+  [n]
+  "Square n"
+  (* n n))
 
 (defn pythagorean?
-	[a b c]
-	"True if aa+bb=cc and a<b<c"
-	(and (< a b c) (= (sq c) (+ (sq a) (sq b)))))
+  [a b c]
+  "True if aa+bb=cc and a<b<c"
+  (and (< a b c) (= (sq c) (+ (sq a) (sq b)))))
 
 (defn triangles
-	[]
-	"Returns infinite sequence of triangle numbers, i.e. 0 1 3 6 10 15 etc."
-	(->> (map #(/ (* % (dec %)) 2) (range)) rest))
+  []
+  "Returns infinite sequence of triangle numbers, i.e. 0 1 3 6 10 15 etc."
+  (->> (map #(/ (* % (dec %)) 2) (range)) rest))
 
 (defn pentagonals
-	[]
-	"Returns infinite sequence of pentagonal numbers, i.e. 0 1 5 12 22 35 etc."
-	(map #(-> % (* 3) dec (* %) (/ 2)) (range)))
+  []
+  "Returns infinite sequence of pentagonal numbers, i.e. 0 1 5 12 22 35 etc."
+  (map #(-> % (* 3) dec (* %) (/ 2)) (range)))
 
-(defn untriangle	; TODO can we replace with triangle? predicate fn?
-	[n]
-	"Returns reverse of triangle number, i.e.
-		1->2 3->3 6->4 10->5 15->6 etc.
-	If result is whole, n is a triangle number"
-	(->> n (* 8) inc (Math/sqrt) dec (* 0.5)))
+(defn untriangle                                            ; TODO can we replace with triangle? predicate fn?
+  [n]
+  "Returns reverse of triangle number, i.e.
+    1->2 3->3 6->4 10->5 15->6 etc.
+  If result is whole, n is a triangle number"
+  (->> n (* 8) inc (Math/sqrt) dec (* 0.5)))
 
 (defn triangle?
-	[n]
-	"True if n is triangle number"
-	(-> n (* 8) inc (Math/sqrt) dec (/ 2) (rem 1) zero?))
+  [n]
+  "True if n is triangle number"
+  (-> n (* 8) inc (Math/sqrt) dec (/ 2) (rem 1) zero?))
 
 (defn pentagonal?
-	[n]
-	"True if n is pentagonal number"
-	(-> n (* 24) inc (Math/sqrt) inc (/ 6) (rem 1) zero?))
+  [n]
+  "True if n is pentagonal number"
+  (-> n (* 24) inc (Math/sqrt) inc (/ 6) (rem 1) zero?))
 
 (defn hexagonal?
-	[n]
-	"True if n is hexagonal number"
-	(-> n (* 8) inc (Math/sqrt) inc (/ 4) (rem 1) zero?))
+  [n]
+  "True if n is hexagonal number"
+  (-> n (* 8) inc (Math/sqrt) inc (/ 4) (rem 1) zero?))
 
 (defn proper-divisors
-	[n]
-	"Returns set of proper divisors of n, e.g. 220 -> 1 2 4 5 10 11 20 22 44 55 110"
-	(if (zero? n) []
-	(->> (range 2 (->> (Math/sqrt n) int inc))
-		(filter #(zero? (rem n %)))
-		(map #(vector % (/ n %)))
-		flatten
-		set
-		(cons 1))))
+  [n]
+  "Returns set of proper divisors of n, e.g. 220 -> 1 2 4 5 10 11 20 22 44 55 110"
+  (if (zero? n) []
+                (->> (range 2 (->> (Math/sqrt n) int inc))
+                     (filter #(zero? (rem n %)))
+                     (map #(vector % (/ n %)))
+                     flatten
+                     set
+                     (cons 1))))
 
 (defn count-divisors
-	[n]
-	"Returns number of divisors for n, uses factor counting method"
-	(->> (factor n) frequencies (map last) (map inc) (apply *)))
+  [n]
+  "Returns number of divisors for n, uses factor counting method"
+  (->> (factor n) frequencies (map last) (map inc) (apply *)))
 
 (defn digits
-	[n]
-	"Sequence of digits in integer n"
-	(map #(- (int %) (int \0)) (str n)))
+  [n]
+  "Sequence of digits in integer n"
+  (map #(- (int %) (int \0)) (str n)))
 
 (defn undigits
-	[coll]
-	"Turn digits into big int, e.g. [1 2 5 8 2] => 12582"
-	(if (seq coll) (bigint (apply str coll)) 0))
+  [coll]
+  "Turn digits into big int, e.g. [1 2 5 8 2] => 12582"
+  (if (seq coll) (bigint (apply str coll)) 0))
 
 (defn sum-digits
-	[n]
-	"Sum of all digits in n"
-	(apply +' (digits n)))
+  [n]
+  "Sum of all digits in n"
+  (apply +' (digits n)))
 
 (defn factorial
-	[n]
-	(reduce *' (range 1 (inc n))))
+  [n]
+  (reduce *' (range 1 (inc n))))
 
 (defn count-combinations
-	[n r]
-	"See calculatorsoup.com/calculators/discretemathematics/combinations.php"
-	(/ (factorial n) (*' (factorial r) (factorial (- n r)))))
+  [n r]
+  "See calculatorsoup.com/calculators/discretemathematics/combinations.php"
+  (/ (factorial n) (*' (factorial r) (factorial (- n r)))))
 
 (defn gcd
-    "Greatest common denominator, Euclid's method"
-    [a b]
-    (cond
-        (> a b)     (recur (- a b) b)
-        (< a b)     (recur a (- b a))
-        :else       a
-        ))
+  "Greatest common denominator, Euclid's method"
+  [a b]
+  (cond
+    (> a b) (recur (- a b) b)
+    (< a b) (recur a (- b a))
+    :else a
+    ))
 
